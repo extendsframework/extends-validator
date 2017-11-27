c6a5c2b00066b0dc108e95c1173c037b2535205a
Vincent van Dijk <info@vincentvandijk.com>
2017-11-27 21:20:13 +0100

    Changed pattern to only allow version 1-5.

--- test/Text/UuidValidatorTest.php
+++ test/Text/UuidValidatorTest.php
@@ -19,9 +19,12 @@ class UuidValidatorTest extends TestCase
     public function testValid(): void
     {
         $validator = new UuidValidator();
-        $result = $validator->validate('db6eb6f2-1dda-4f06-a995-1fd1aca99e1f');
 
-        $this->assertTrue($result->isValid());
+        $this->assertTrue($validator->validate('db6eb6f2-1dda-1f06-a995-1fd1aca99e1f')->isValid()); // Version 1
+        $this->assertTrue($validator->validate('db6eb6f2-1dda-2f06-a995-1fd1aca99e1f')->isValid()); // Version 2
+        $this->assertTrue($validator->validate('db6eb6f2-1dda-3f06-a995-1fd1aca99e1f')->isValid()); // Version 3
+        $this->assertTrue($validator->validate('db6eb6f2-1dda-4f06-a995-1fd1aca99e1f')->isValid()); // Version 4
+        $this->assertTrue($validator->validate('db6eb6f2-1dda-5f06-a995-1fd1aca99e1f')->isValid()); // Version 5
     }
 
     /**
@@ -32,12 +35,12 @@ class UuidValidatorTest extends TestCase
      * @covers \ExtendsFramework\Validator\Text\UuidValidator::validate()
      * @covers \ExtendsFramework\Validator\Text\UuidValidator::getTemplates()
      */
-    public function testCanNotAssertInvalidUuid(): void
+    public function testInvalid(): void
     {
         $validator = new UuidValidator();
-        $result = $validator->validate('foo-bar-baz');
 
-        $this->assertFalse($result->isValid());
+        $this->assertFalse($validator->validate('foo-bar-baz')->isValid());
+        $this->assertFalse($validator->validate('db6eb6f2-1dda-6f06-a995-1fd1aca99e1f')->isValid()); // Version unknown
     }
 
     /**
