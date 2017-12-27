28e4c4abb19a09dfc9e00f9c556482fd6b3d1f91
Vincent van Dijk <info@vincentvandijk.com>
2017-12-27 14:31:42 +0100

    Moved validator code to separate method.

--- src/Framework/ServiceLocator/Factory/Validator/ValidatorFactory.php
+++ src/Framework/ServiceLocator/Factory/Validator/ValidatorFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace ExtendsFramework\Validator\Framework\ServiceLocator\Factory\Validator;
 
 use ExtendsFramework\ServiceLocator\Resolver\Factory\ServiceFactoryInterface;
+use ExtendsFramework\ServiceLocator\ServiceLocatorException;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 use ExtendsFramework\Validator\ContainerValidator;
 use ExtendsFramework\Validator\ValidatorInterface;
@@ -13,16 +14,30 @@ class ValidatorFactory implements ServiceFactoryInterface
     /**
      * @inheritDoc
      */
-    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): ValidatorInterface
+    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): object
     {
         $container = new ContainerValidator();
         foreach ($extra['validators'] ?? [] as $validator) {
             $container->addValidator(
-                $serviceLocator->getService($validator['name'], $validator['options'] ?? []),
+                $this->getValidator($serviceLocator, $validator['name'], $validator['options'] ?? []),
                 $validator['interrupt'] ?? false
             );
         }
 
         return $container;
+    }
+
+    /**
+     * Get validator for name.
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param string                  $name
+     * @param array                   $options
+     * @return ValidatorInterface
+     * @throws ServiceLocatorException
+     */
+    protected function getValidator(ServiceLocatorInterface $serviceLocator, string $name, array $options): object
+    {
+        return $serviceLocator->getService($name, $options);
     }
 }
